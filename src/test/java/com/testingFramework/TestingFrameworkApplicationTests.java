3aad15b4395ff12ac3a4c825663aa4d6cf05b9d4
Kritika Chakraborty <kritika7.kc@gmail.com>
2026-01-15 01:03:50 +0530

    Create TestingFrameworkApplicationTests.java

---
+++ src/test/java/com/testingFramework/TestingFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kritika.testingFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
